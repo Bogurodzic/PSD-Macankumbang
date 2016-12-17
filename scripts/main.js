bbc7e4175c331e93957f82e410e189c248bac7df
Kamil Jarzab <jarzbkamil@gmail.com>
2016-12-17 16:43:46 +0100

    Finished Header-Section

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+/* var hamburger = document.getElementById("hamburger");
+var navigation = document.getElementById("hamburger-navigation");
+
+hamburger.addEventListener("click", function(){
+	navigation.style.display = "block";
+}); */
