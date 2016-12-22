35c90ece4a472c81faaac9a9e50fce0158b9b6dc
Kamil Jarzab <jarzbkamil@gmail.com>
2016-12-22 15:35:43 +0100

    Added filter option

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,55 @@
-/* var hamburger = document.getElementById("hamburger");
-var navigation = document.getElementById("hamburger-navigation");
+var categories = document.getElementById("works-categories").children,
+	catLength = categories.length
 
-hamburger.addEventListener("click", function(){
-	navigation.style.display = "block";
-}); */
+console.log(categories[0]);
+
+for(var i=0; i<catLength; i++){
+	
+	var category = categories[i];
+
+	category.addEventListener("click", function(e){
+
+		e.preventDefault();
+
+		var categoryAttr = this.getAttribute("data-filter");
+
+		filterPortfolio(categoryAttr);
+
+	});
+
+}
+
+
+var portfolio = document.querySelector(".works-portfolio").children;
+console.log(portfolio);
+
+function filterPortfolio(attrToFilter){
+
+	showAll();
+
+	[].forEach.call(portfolio, function(element){
+
+		var elementAttr = element.getAttribute("data-filter");
+
+		if (attrToFilter === "All"){
+			showAll();
+			return;
+		}
+
+		if (attrToFilter !== elementAttr){
+			element.style.display = "none";
+			element.style.opacity = 0;
+		}
+	});
+
+}
+
+
+function showAll(){
+
+	[].forEach.call(portfolio, function(element){
+		element.style.display = "block";
+		element.style.opacity = 1;
+	});
+
+}
