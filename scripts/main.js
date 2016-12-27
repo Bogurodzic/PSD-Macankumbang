3017d9a2c4db5951469f55ff631137b60cd9274e
Kamil Jarzab <jarzbkamil@gmail.com>
2016-12-27 14:28:46 +0100

    Added testimonial slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,107 @@
-var categories = document.getElementById("works-categories").children,
-	catLength = categories.length
+$(document).ready(function($) {
 
-for(var i=0; i<catLength; i++){
-	
-	var category = categories[i];
+	$('.my-slider').unslider();
 
-	category.addEventListener("click", function(e){
+	var categories = document.getElementById("works-categories").children,
+		catLength = categories.length
 
-		e.preventDefault();
+	for(var i=0; i<catLength; i++){
+		
+		var category = categories[i];
 
-		var categoryAttr = this.getAttribute("data-filter");
+		category.addEventListener("click", function(e){
 
-		filterPortfolio(categoryAttr);
+			e.preventDefault();
+
+			var categoryAttr = this.getAttribute("data-filter");
+
+			filterPortfolio(categoryAttr);
+
+		});
+
+	}
+
+	var portfolio = document.querySelector(".works-portfolio").children;
+
+	function filterPortfolio(attrToFilter){
+
+		showAll();
+
+		[].forEach.call(portfolio, function(element){
+
+			var elementAttr = element.getAttribute("data-filter");
+
+			if (attrToFilter === "All"){
+				showAll();
+				return;
+			}
+
+			if (attrToFilter !== elementAttr){
+				element.style.display = "none";
+				element.style.opacity = 0;
+			}
+		});
+
+	}
+
+	function showAll(){
+
+		[].forEach.call(portfolio, function(element){
+			element.style.display = "block";
+			element.style.opacity = 1;
+		});
+
+	}
 
 	});
+	var categories = document.getElementById("works-categories").children,
+		catLength = categories.length
+
+	for(var i=0; i<catLength; i++){
+		
+		var category = categories[i];
+
+		category.addEventListener("click", function(e){
+
+			e.preventDefault();
+
+			var categoryAttr = this.getAttribute("data-filter");
+
+			filterPortfolio(categoryAttr);
+
+		});
+
+	}
+
+	var portfolio = document.querySelector(".works-portfolio").children;
+
+	function filterPortfolio(attrToFilter){
+
+		showAll();
+
+		[].forEach.call(portfolio, function(element){
+
+			var elementAttr = element.getAttribute("data-filter");
+
+			if (attrToFilter === "All"){
+				showAll();
+				return;
+			}
+
+			if (attrToFilter !== elementAttr){
+				element.style.display = "none";
+				element.style.opacity = 0;
+			}
+		});
+
+	}
+
+	function showAll(){
+
+		[].forEach.call(portfolio, function(element){
+			element.style.display = "block";
+			element.style.opacity = 1;
+		});
 
 }
 
-var portfolio = document.querySelector(".works-portfolio").children;
-
-function filterPortfolio(attrToFilter){
-
-	showAll();
-
-	[].forEach.call(portfolio, function(element){
-
-		var elementAttr = element.getAttribute("data-filter");
-
-		if (attrToFilter === "All"){
-			showAll();
-			return;
-		}
-
-		if (attrToFilter !== elementAttr){
-			element.style.display = "none";
-			element.style.opacity = 0;
-		}
-	});
-
-}
-
-function showAll(){
-
-	[].forEach.call(portfolio, function(element){
-		element.style.display = "block";
-		element.style.opacity = 1;
-	});
-
-}
